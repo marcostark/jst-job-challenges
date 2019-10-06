8db8b764e8db3e5c57c56b590c392b589015e8a2
stark <mrcs.stark@gmail.com>
2019-10-05 23:51:14 -0300

    create news models

---
+++ mobile/justamobile/app/src/main/java/br/com/marcosouza/justamobile/model/NewsResponse.java
@@ -0,0 +1,44 @@
+package br.com.marcosouza.justamobile.model;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
+public class NewsResponse {
+
+    @SerializedName("status")
+    @Expose
+    private String status;
+    @SerializedName("totalResults")
+    @Expose
+    private Integer totalResults;
+    @SerializedName("articles")
+    @Expose
+    private List<NewsArticles> articles = null;
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public Integer getTotalResults() {
+        return totalResults;
+    }
+
+    public void setTotalResults(Integer totalResults) {
+        this.totalResults = totalResults;
+    }
+
+    public List<NewsArticles> getArticles() {
+        return articles;
+    }
+
+    public void setArticles(List<NewsArticles> articles) {
+        this.articles = articles;
+    }
+
+}
