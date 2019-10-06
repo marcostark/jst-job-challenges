fc46b030be235784024fc3d0b6e092cc5c410c50
stark <mrcs.stark@gmail.com>
2019-10-06 18:41:42 -0300

    Refactor models

---
+++ mobile/justamobile/app/src/main/java/br/com/marcosouza/justamobile/model/RecyclingCompany.java
@@ -0,0 +1,96 @@
+package br.com.marcosouza.justamobile.model;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class RecyclingCompany {
+
+    @SerializedName("id")
+    @Expose
+    private Integer id;
+    @SerializedName("name")
+    @Expose
+    private String name;
+    @SerializedName("thumb")
+    @Expose
+    private String thumb;
+    @SerializedName("address")
+    @Expose
+    private String address;
+    @SerializedName("phone")
+    @Expose
+    private String phone;
+    @SerializedName("description")
+    @Expose
+    private String description;
+    @SerializedName("lat")
+    @Expose
+    private String lat;
+    @SerializedName("lon")
+    @Expose
+    private String lon;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getThumb() {
+        return thumb;
+    }
+
+    public void setThumb(String thumb) {
+        this.thumb = thumb;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getLat() {
+        return lat;
+    }
+
+    public void setLat(String lat) {
+        this.lat = lat;
+    }
+
+    public String getLon() {
+        return lon;
+    }
+
+    public void setLon(String lon) {
+        this.lon = lon;
+    }
+}
---
+++ mobile/justamobile/app/src/main/java/br/com/marcosouza/justamobile/model/RecyclingCompanyResponse.java
@@ -0,0 +1,76 @@
+package br.com.marcosouza.justamobile.model;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
+public class RecyclingCompanyResponse {
+
+    @SerializedName("atual_page")
+    @Expose
+    private Integer atualPage;
+    @SerializedName("num_pages")
+    @Expose
+    private Integer numPages;
+    @SerializedName("num_itens")
+    @Expose
+    private Integer numItens;
+    @SerializedName("has_previous")
+    @Expose
+    private Boolean hasPrevious;
+    @SerializedName("has_next")
+    @Expose
+    private Boolean hasNext;
+    @SerializedName("results")
+    @Expose
+    private List<RecyclingCompany> results = null;
+
+    public Integer getAtualPage() {
+        return atualPage;
+    }
+
+    public void setAtualPage(Integer atualPage) {
+        this.atualPage = atualPage;
+    }
+
+    public Integer getNumPages() {
+        return numPages;
+    }
+
+    public void setNumPages(Integer numPages) {
+        this.numPages = numPages;
+    }
+
+    public Integer getNumItens() {
+        return numItens;
+    }
+
+    public void setNumItens(Integer numItens) {
+        this.numItens = numItens;
+    }
+
+    public Boolean getHasPrevious() {
+        return hasPrevious;
+    }
+
+    public void setHasPrevious(Boolean hasPrevious) {
+        this.hasPrevious = hasPrevious;
+    }
+
+    public Boolean getHasNext() {
+        return hasNext;
+    }
+
+    public void setHasNext(Boolean hasNext) {
+        this.hasNext = hasNext;
+    }
+
+    public List<RecyclingCompany> getResults() {
+        return results;
+    }
+
+    public void setResults(List<RecyclingCompany> results) {
+        this.results = results;
+    }
+}
