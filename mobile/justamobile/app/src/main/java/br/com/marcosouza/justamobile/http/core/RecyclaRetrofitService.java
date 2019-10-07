e4a2a194e84a766c8cdc619584ce2c2457787604
stark <mrcs.stark@gmail.com>
2019-10-07 13:12:17 -0300

    Update gitignore

--- mobile/justamobile/app/src/main/java/br/com/marcosouza/justamobile/http/core/RecyclaRetrofitService.java
+++ mobile/justamobile/app/src/main/java/br/com/marcosouza/justamobile/http/core/RecyclaRetrofitService.java
@@ -3,6 +3,8 @@ package br.com.marcosouza.justamobile.http.core;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
+/*TODO mudar para tratamento de url dinamica com okhttp*/
+
 public class RecyclaRetrofitService {
     private static Retrofit retrofit = new Retrofit.Builder()
             .baseUrl("http://192.168.100.3:8000/")
