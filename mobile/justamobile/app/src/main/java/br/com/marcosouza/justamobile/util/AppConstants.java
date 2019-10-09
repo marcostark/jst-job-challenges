9cb11314c42087c16cf22c97b64d41504a1c1048
stark <mrcs.stark@gmail.com>
2019-10-09 01:27:07 -0300

    Add constants class

---
+++ mobile/justamobile/app/src/main/java/br/com/marcosouza/justamobile/util/AppConstants.java
@@ -0,0 +1,24 @@
+package br.com.marcosouza.justamobile.util;
+
+public final class AppConstants {
+
+    // Weather Constants
+
+    public static final String WEATHER_ENDPOINT = "http://api.openweathermap.org/data/2.5/weather?";
+
+    public static final String API_KEY = "690cac557da1ff012090de70f05d808b";
+
+    public static final String UNITS = "metric";
+
+    public static final String CITY = "Serra Talhada";
+
+
+    // News Constants
+
+    public static final String NEWS_ENDPOINT = "https://newsapi.org/v2/everything";
+
+
+    // Not publicly instantiable
+    private AppConstants() {}
+
+}
