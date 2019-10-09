18e38410402c16238db44a863f1a22a1a2f8eb18
Marcos <marcosasouza.ti@gmail.com>
2019-10-09 16:17:30 -0300

    Update AppConstants class

--- mobile/justamobile/app/src/main/java/br/com/marcosouza/justamobile/util/AppConstants.java
+++ mobile/justamobile/app/src/main/java/br/com/marcosouza/justamobile/util/AppConstants.java
@@ -3,7 +3,6 @@ package br.com.marcosouza.justamobile.util;
 public final class AppConstants {
 
     // Weather Constants
-
     public static final String WEATHER_ENDPOINT = "http://api.openweathermap.org/data/2.5/weather?";
 
     public static final String API_KEY = "690cac557da1ff012090de70f05d808b";
@@ -14,7 +13,6 @@ public final class AppConstants {
 
 
     // News Constants
-
     public static final String NEWS_ENDPOINT = "https://newsapi.org/v2/everything";
 
 
